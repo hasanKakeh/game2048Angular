b87ca166c4b485dcfa4696a8cc2a108f38d977d4
hassan kakeh <hassan.kakah@preneom.com>
2022-05-19 12:54:51 +0300

    add sw

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { TileComponent } from './components/tile/tile.component';
 import { GridComponent } from './components/grid/grid.component';
 import { CellComponent } from './components/cell/cell.component';
 import { EventHandlerDirective } from './directives/event-handler.directive';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,12 @@ import { EventHandlerDirective } from './directives/event-handler.directive';
     CellComponent,
     EventHandlerDirective,
   ],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: environment.production,
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [],
   bootstrap: [AppComponent],
 })
